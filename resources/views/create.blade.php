46ae07d396269f185898c52f6027e62b28dc82a5
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-11 21:24:43 +0545

    handling validation error in the frontend

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,12 +1,27 @@
 <div>
 
 <h1>Add data to the database</h1>
+
+
+{{-- showing validation errors --}}
+@if($errors -> any())
+<ul>
+    @foreach ($errors->all() as  $error)
+        <li>{{ $error }}</li>
+        
+    @endforeach
+</ul>
+
+@endif
+
+
+
 <form method="POST" action="{{ route('store') }}">
  @csrf
 
     <label for="name">Product Name</label>
-    <input type="text" id="name" name="name">
-
+    <input type="text" id="name" name="name" value="{{ old('name') }}">
+{{-- value="{{ old('name') }} to keep the old value when the validation failed --}}
     <label for="description">product description</label>
     <input type="text" id="description" name="description">
 

251089712b9e6dac6f6b4bd1a94e311d32fc7af0
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-11 17:12:21 +0545

    added csrf token

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,7 +1,7 @@
 <div>
 
 <h1>Add data to the database</h1>
-<form action="" method="POST" action="{{ route('store') }}>
+<form method="POST" action="{{ route('store') }}">
  @csrf
 
     <label for="name">Product Name</label>
@@ -12,6 +12,7 @@
 
     <label for="price">Price</label>
     <input type="number" id="price" name="price">
+   
 <button>Add</button>
 
 

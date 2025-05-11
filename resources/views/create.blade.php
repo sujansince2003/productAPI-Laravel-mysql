7e968a7b5027e5ad1634014b3165eabac96a9057
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-11 16:56:52 +0545

    add product blade template

---
+++ resources/views/create.blade.php
@@ -0,0 +1,21 @@
+<div>
+
+<h1>Add data to the database</h1>
+<form action="" method="POST" action="{{ route('store') }}>
+ @csrf
+
+    <label for="name">Product Name</label>
+    <input type="text" id="name" name="name">
+
+    <label for="description">product description</label>
+    <input type="text" id="description" name="description">
+
+    <label for="price">Price</label>
+    <input type="number" id="price" name="price">
+<button>Add</button>
+
+
+</form>
+
+
+</div>

975e82545afb278547c495d84e79570e1823248a
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-11 16:55:57 +0545

    product controller

---
+++ app/Http/Controllers/productController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class productController extends Controller
+{
+    public function index(){
+   $products= product::all();
+
+//   dd($products);
+        return view('product',[
+            "products"=>$products
+        ]);
+    }
+
+        public function create(){
+            return view('create');
+        }
+
+       public function store(Request $request){
+    $name=$request->input('name');
+    $description=$request->input('description');
+    $price=$request->input('price');
+    $product=new Product;
+    $product->name=$name;
+    $product->description=$description;
+    $product->price=$price;
+    $product->save();
+    return redirect()->route('product');
+       }
+
+}

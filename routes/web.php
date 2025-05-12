f99713a082efd102b5d4067bc63efb23d72bb735
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-12 15:00:32 +0545

    defined route for single product

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('/', function () {
 Route::get("/products",[productController::class,"index"])->name("products");
 Route::get("/products/create",[productController::class,"create"])->name("create");
 Route::post("/products/store",[productController::class,"store"])->name("store");
+Route::get("/products/{id}",[productController::class,"viewProduct"])->name("viewProduct")->where("id","[0-9]+");

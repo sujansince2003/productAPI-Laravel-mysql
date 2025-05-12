99f0238a14984c01078044bbaa7b16fb280beac1
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-12 15:00:03 +0545

    redirect to single product

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,6 +3,9 @@
 <a href="{{ route('create') }}">add new product</a>
 
 @foreach ($products as $product)
+<br>
+
+<a href="{{ route('viewProduct',[$product->id]) }}">{{$product->name}}</a>
     <h1>{{ $product->name }}</h1>
     <h2>price:{{ $product->price }}</h2>
     <h3>description:{{ $product->description }}</h3>

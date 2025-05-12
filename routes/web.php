c8316095975805e6010b3cbc67c99dcc461c0afb
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-12 21:17:40 +0545

    added relevant routes

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,8 @@ Route::get("/products",[productController::class,"index"])->name("products");
 Route::get("/products/create",[productController::class,"create"])->name("create");
 Route::post("/products/store",[productController::class,"store"])->name("store");
 Route::get("/products/{id}",[productController::class,"viewProduct"])->name("viewProduct")->where("id","[0-9]+");
+Route::get("/products/{id}/edit",[productController::class,"editProduct"])->name("editProduct")->where("id","[0-9]+");
+
+Route::patch("/products/{id}",[productController::class,"updateProduct"])->name("updateProduct")->where("id","[0-9]+"); 
+
+Route::delete("/products/{id}",[productController::class,"deleteProduct"])->name("deleteProduct")->where("id","[0-9]+");
